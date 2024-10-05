8d8d35bd30d4f9f8c024ff768b719345b9f8d489
scudelia <sabres@ac.auone-net.jp>
2024-10-05 15:48:25 +0000

    選手所属テーブルの追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MatchCategorySeeder::class,
             MatchScheduleSeeder::class,
+            PlayerAffiliationSeeder::class,
             PlayerSeeder::class,
             SeasonSeeder::class,
             TeamSeeder::class,
