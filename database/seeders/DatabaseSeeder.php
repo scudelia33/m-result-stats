874b1297f21b5569bd49341d3fbb877e7d0dffca
scudelia <sabres@ac.auone-net.jp>
2024-10-05 14:16:52 +0000

    選手マスタの追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MatchCategorySeeder::class,
             MatchScheduleSeeder::class,
+            PlayerSeeder::class,
             SeasonSeeder::class,
             TeamSeeder::class,
         ]);
