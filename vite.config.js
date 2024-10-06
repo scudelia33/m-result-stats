739f29dccccc5fb14ff16d85f4b459748d9afb4b
scudelia <sabres@ac.auone-net.jp>
2024-10-06 11:06:41 +0000

    bootstrapの導入&確認ページの追加

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
@@ -16,5 +19,5 @@ export default defineConfig({
         watch: {
             usePolling: true,
         },
-    },    
+    },
 });
