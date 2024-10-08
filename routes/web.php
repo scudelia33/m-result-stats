edb92c46b9d2189ac3bdf909aaf6b51d222df11c
scudelia <sabres@ac.auone-net.jp>
2024-10-08 11:22:28 +0000

    選手所属一覧画面の追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PlayerAffiliationController;
 use App\Http\Controllers\TeamController;
+use App\Http\Middleware\PlayerAffiliationIndexMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +11,13 @@ Route::get('/', function () {
 
 // チーム
 route::get('/teams', [TeamController::class, 'index']);
+
+// 選手所属
+route::get('/player-affiliations', [PlayerAffiliationController::class, 'index'])
+    ->middleware([
+        PlayerAffiliationIndexMiddleware::class
+    ])->
+    name('player-affiliations');
 
 Route::get('/bootstrap', function () {
     return view('bootstrap');
