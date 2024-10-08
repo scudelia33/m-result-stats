7f3b8ac550bb3623937c62b695ee2ba7cb00faf0
scudelia <sabres@ac.auone-net.jp>
2024-10-08 11:17:53 +0000

    選手モデルに選手名を取得するメソッドを追加

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,4 +14,17 @@ class Player extends Model
 
     // 主キー
     protected $primaryKey = 'player_id';
+
+    // ====================
+    // アクセサ
+    // ====================
+    /**
+     * 選手名(名字+名前)を取得する
+     */
+    public function playerName(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => $attributes['player_last_name'] . ' ' . $attributes['player_first_name'],
+        );
+    }
 }
