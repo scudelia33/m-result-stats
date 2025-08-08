127b7869691081aa5ae606bcba14824e1fab0005
scudelia <sabres@ac.auone-net.jp>
2025-08-08 18:55:18 +0000

    テーブルヘッダーを固定するCSSを追加し、スクロール可能なテーブルを実装

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,19 +1,43 @@
-<div @class([
-    'table-responsive',
-])>
+<style>
+/* テーブルヘッダー固定用CSS */
+.sticky-header thead th {
+    position: sticky;
+    top: 0;
+    background-color: #fff;
+    z-index: 10;
+    /* ダークモードでも見やすいようにボーダーを追加 */
+    border-bottom: 1px solid #444;
+    color: inherit;
+}
+@media (prefers-color-scheme: dark) {
+  .sticky-header thead th {
+    background-color: #222;
+    color: #fff;
+  }
+}
+.sticky-header tbody td {
+    background: inherit;
+    position: relative;
+    z-index: 1;
+}
+.table-scroll {
+    max-height: calc(100vh - 200px); /* 200pxは必要に応じて調整 */
+    overflow-y: auto;
+}
+</style>
+<div class="table-responsive table-scroll">
     <caption>{{ $title }}</caption>
-
     <table @class([
         'table',
         'table-hover',
         'caption-top',
+        'sticky-header',
     ])>
         <thead>
             <tr>
                 {{ $header }}
             </tr>
         </thead>
-
         <tbody>
             {{ $body }}
         </tbody>
