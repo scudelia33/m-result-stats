417a449991df1a8c05eaba5aa8503b935ddb8f5e
scudelia <sabres@ac.auone-net.jp>
2025-09-21 21:23:39 +0900

    チームテーブルに背景色とグラフ用の色の列を追加

--- src/resources/views/team/index.blade.php
+++ src/resources/views/team/index.blade.php
@@ -16,6 +16,8 @@
             <th scope="col">@sortablelink('team_id', 'ID')</th>
             <th scope="col">@sortablelink('team_name', 'チーム名')</th>
             <th scope="col">{{ __('team.team_name_shortened') }}</th>
+            <th scope="col">{{ __('team.background_color') }}</th>
+            <th scope="col">{{ __('team.graph_color') }}</th>
         </x-slot>
 
         <x-slot:body>
@@ -26,6 +28,8 @@
                 <x-team-name :team-name="$team->team_name" :team-color="$team->background_color" />
                 {{-- 短縮名（チーム名の右側） --}}
                 <td class="text-center">{{ $team->team_name_shortened }}</td>
+                <td class="text-center">{{ $team->background_color }}</td>
+                <td class="text-center">{{ $team->graph_color }}</td>
             </tr>
             @endforeach
         </x-slot>
