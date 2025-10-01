b129b50e1971c537fee535e3e70241353dd3b684
scudelia <sabres@ac.auone-net.jp>
2025-10-02 00:11:22 +0900

    MatchScheduleControllerにMatchScheduleServiceを導入し、試合日程データの準備ロジックを移行。MatchScheduleIndexMiddlewareを削除し、ルーティングからも除去。

--- src/app/Http/Controllers/MatchScheduleController.php
+++ src/app/Http/Controllers/MatchScheduleController.php
@@ -4,19 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use App\Services\MatchScheduleService;
 
 /**
  * 試合日程コントローラ
  */
 class MatchScheduleController extends Controller
 {
+    private MatchScheduleService $service;
+
+    public function __construct(MatchScheduleService $service)
+    {
+        $this->service = $service;
+    }
     /**
+     * 試合日程一覧画面を表示します。
+     * サービスでデータ準備を行い、ビューに渡します。
      *
+     * @param Request $request
+     * @return View
      */
     public function index(Request $request): View
     {
-        return View('match-schedule.index',
-            compact('request'),
-        );
+        $request = $this->service->prepareIndexData($request);
+
+        return View('match-schedule.index', compact('request'));
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\TeamPointChartController;
 use App\Http\Controllers\TeamRankingController;
 use App\Http\Controllers\TeamStatsController;
-use App\Http\Middleware\MatchScheduleIndexMiddleware;
 use App\Http\Middleware\MatchResultIndexMiddleware;
 use App\Http\Middleware\SeasonPlayerRankingIndexMiddleware;
 use App\Http\Middleware\AllPlayerRankingIndexMiddleware;
@@ -31,10 +30,7 @@ route::get('/player-affiliations', [PlayerAffiliationController::class, 'index']
 
 // 試合日程
 route::get('/match-schedules', [MatchScheduleController::class, 'index'])
-    ->middleware([
-        MatchScheduleIndexMiddleware::class
-    ])->
-    name('match-schedules');
+    ->name('match-schedules');
 
 // 試合日程
 route::get('/match-results', [MatchResultController::class, 'index'])
